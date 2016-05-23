548e67930482a25afe060fa496ace78fe52172f2
Pascal <Pascal@PascalPC>
2016-05-23 17:38:29 +0200

    Finished first Version of GUI

--- MüsCustoms/src/application/LoginController.java
+++ MüsCustoms/src/application/LoginController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
