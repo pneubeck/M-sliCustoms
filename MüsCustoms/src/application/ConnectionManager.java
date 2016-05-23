79baf860a135f14f314bd3ba121d0a62e629f875
MG <email@email.de>
2016-05-23 17:43:03 +0200

    libraries added

---
+++ MüsCustoms/src/application/ConnectionManager.java
@@ -0,0 +1,10 @@
+package application;
+import com.sap.conn.jco.*;
+public class ConnectionManager {
+	JCo.;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
